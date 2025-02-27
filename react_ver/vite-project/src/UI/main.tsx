89bcf1cda6a068aff4b88b70fa06469c7783ceaa
wjlee <000jun2626@gmail.com>
2025-02-27 21:55:56 +0000

    라즈베리파이 환경을위한 주석석

--- react_ver/vite-project/src/UI/main.tsx
+++ react_ver/vite-project/src/UI/main.tsx
@@ -1,9 +1,10 @@
-import { StrictMode } from 'react';
-import { createRoot } from 'react-dom/client';
-import './index.css';
-import App from './App.tsx';
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.tsx";
 
-createRoot(document.getElementById('root')!).render(
+//npm run build 후, npm run dev:electron 하기. dist-react 폴더에 있는 index.html 파일이 electron에 로드되야 화면에 나타남
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <App />
   </StrictMode>,
