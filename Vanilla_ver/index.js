7991de5a9d21352575f1fd4fa69d9e8636ed6149
wjlee <000jun2626@gmail.com>
2025-03-01 14:19:46 +0900

    feat: 첫화면 UI 수정(버튼 엘리먼트&lottie 시각적 동기화)

--- Vanilla_ver/index.js
+++ Vanilla_ver/index.js
@@ -1,23 +1,44 @@
 
-lottie.loadAnimation({
-    container: document.getElementById('pumpkin'), // 애니메이션을 표시할 컨테이너
-    renderer: 'svg',
-    loop: true,
-    autoplay: true,
-    path: './lottie/pumpkin.json' // Lottie 애니메이션 JSON 파일 경로
-});
-
-lottie.loadAnimation({
-    container: document.getElementById('welcome'), // 애니메이션을 표시할 컨테이너
-    renderer: 'svg',
-    loop: true,
-    autoplay: true,
-    path: './lottie/welcome.json' // Lottie 애니메이션 JSON 파일 경로.
-});
 
 document.getElementById("start-button").addEventListener(`click`, () => {
     document.body.classList.add(`fade-out`);
     setTimeout(() => {
-        window.location.href = `./orderPage.html`;
+        window.location.href = `./page/orderPage.html`;
     }, 2000);
 });
+
+window.onload = function () {
+    // 모든 리소스가 로드된 후에 요소들을 표시
+    const welcomeAnimation = lottie.loadAnimation({
+        container: document.getElementById('welcome'),
+        renderer: 'svg',
+        loop: true,
+        autoplay: true,
+        path: './lottie/welcome.json',
+    });
+
+    const pumpkinAnimation = lottie.loadAnimation({
+        container: document.getElementById('pumpkin'),
+        renderer: 'svg',
+        loop: true,
+        autoplay: true,
+        path: './lottie/pumpkin.json',
+    });
+
+    // // Lottie 애니메이션이 로드된 후에 요소들을 표시
+    // welcomeAnimation.addEventListener('DOMLoaded', () => {
+    //     document.getElementById('welcome').classList.remove('hidden');
+    // });
+
+    // pumpkinAnimation.addEventListener('DOMLoaded', () => {
+    //     document.getElementById('pumpkin').classList.remove('hidden');
+    // });
+
+    // 모든 애니메이션이 로드된 후에 버튼을 표시
+    Promise.all([
+        new Promise(resolve => welcomeAnimation.addEventListener('DOMLoaded', resolve)),
+        new Promise(resolve => pumpkinAnimation.addEventListener('DOMLoaded', resolve)),
+    ]).then(() => {
+        document.getElementById('start-button').classList.remove('hidden');
+    });
+};
