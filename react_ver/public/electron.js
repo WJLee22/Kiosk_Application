850e787dba50314593f7772b49a253e47c11b008
wjlee <000jun2626@gmail.com>
2025-03-03 05:08:10 +0900

    docs

--- react_ver/public/electron.js
+++ react_ver/public/electron.js
@@ -11,6 +11,9 @@ app.commandLine.appendSwitch('disable-software-rasterizer');
 let mainWindow = null;
 
 async function createWindow() {
+    // electron-is-dev 모듈을 사용하면 애플리케이션이 개발 모드에서 실행 중인지 배포 모드에서 실행 중인지 쉽게 판단할 수 있음.
+    // const isDev = (await import('electron-is-dev')).default; => 
+
     const isDev = (await import('electron-is-dev')).default;
 
     mainWindow = new BrowserWindow({
@@ -25,8 +28,13 @@ async function createWindow() {
     });
 
     mainWindow.loadURL(
+        //React App은 DevServer(localhost:3000에서 실행중,,)를 통해 개발된 수정사항만을 즉시 build하고 화면에 적용시키는 Hot Reload 기능이 존재한다.
+        //때문에, Develop Mode 에서는 DevServer를 띄우고 Election은 이 DevServer와 URL로 연결하여, 해당 DevServer에서 실행중인 리액트 앱을 일렉트론 앱에 로드하여 일렉트론 개발을 진행하고, 실행파일 배포단계인 Production Mode 일때는 Build한 리액트 프로젝트를 Electron 에 탑재(즉 Electron 앱으로 한번 더 패키징)하여 쉽게 개발하고, 배포할 수 있게 된다.
+
         //개발 모드일 때는 localhost:3000에서 로드하고, 배포 모드일 때는 빌드된 HTML 파일을 로드.
-        //즉, npm start로 실행할 때는 localhost:3000에서 로드하고, npm run build로 빌드한 후에는 빌드된 HTML 파일을 로드
+        //즉, npm start로 실행할 때는 localhost:3000에서 로드하고, npm run build로 빌드한 후에는 빌드된 HTML 파일을 로드.
+        // isDev 변수가 true일 때는 개발 모드로 간주하여 http://localhost:3000 URL을 로드하고, false일 때는 배포 모드로 간주하여 빌드된 HTML 파일을 로드
+
         isDev
             ? 'http://localhost:3000'
             : `file://${path.join(__dirname, '../build/index.html')}`
